0a3824999bd046ced4ade90a1f09f3c73bdbec06
Alizadekh <aykhanalizadekh@gmail.com>
2024-07-13 00:34:23 +0400

    Create Reusable Table Component

---
+++ src/Components/Reusable/Table.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ReusableTable = ({ columns, data, className }) => {
+  return (
+    <table className={className}>
+      <thead>
+        <tr>
+          {columns.map((col) => (
+            <th key={col.key}>{col.title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columns.map((col) => (
+              <td key={col.key}>
+                {col.render ? col.render(row[col.key], row) : row[col.key]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ReusableTable;
