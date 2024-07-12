7f13c06cd811dd3f25aa9c7ac7c923a6ceb1fd89
Zibeydee <zibeydeceferli2012@gmail.com>
2024-07-12 14:02:07 +0400

    navbar connected to orderhistory page

--- src/Pages/OrderHistoryPage.jsx
+++ src/Pages/OrderHistoryPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SubNavOrderHistory from '../Components/OrderHistory/SubNavOrderHistory'
 import OrderHistory from '../Components/OrderHistory/OrderHistory'
+import NavBar from "../Components/NavBar/NavBar"
 
 function OrderHistoryPage() {
   return (
     <div>
+      <NavBar/>
         <SubNavOrderHistory/>
         <OrderHistory/>
     </div>
