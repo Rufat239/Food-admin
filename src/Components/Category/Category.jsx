117c0d5c486dbe4c3d2740ef1ddd32453c06a94a
RasulGv <garavaliyevrasul531@gmail.com>
2024-07-12 13:49:10 +0400

    deleted all categories code

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,49 +1,49 @@
-import React from 'react'
-import '../../Style/categoryCards.css'
+// import React from 'react'
+// import '../../Style/categoryCards.css'
 
 
-const data = [
-  { id: 9177, image: "", name: 'Pizza', slug: 'yummy-pizza' },
-  { id: 9178, image: "", name: 'Sendvic', slug: 'sendvic' },
-  { id: 9177, image: "", name: 'Fries', slug: 'fries' },
-  { id: 9177, image: "", name: 'Pizza', slug: 'yummy-pizza' },
-  { id: 9178, image: "", name: 'Sendvic', slug: 'sendvic' },
-  { id: 9177, image: "", name: 'Fries', slug: 'fries' },
-  { id: 9177, image: "", name: 'Pizza', slug: 'yummy-pizza' },
-  { id: 9178, image: "", name: 'Sendvic', slug: 'sendvic' },
-  { id: 9177, image: "", name: 'Fries', slug: 'fries' },
-];
+// const data = [
+//   { id: 9177, image: "", name: 'Pizza', slug: 'yummy-pizza' },
+//   { id: 9178, image: "", name: 'Sendvic', slug: 'sendvic' },
+//   { id: 9177, image: "", name: 'Fries', slug: 'fries' },
+//   { id: 9177, image: "", name: 'Pizza', slug: 'yummy-pizza' },
+//   { id: 9178, image: "", name: 'Sendvic', slug: 'sendvic' },
+//   { id: 9177, image: "", name: 'Fries', slug: 'fries' },
+//   { id: 9177, image: "", name: 'Pizza', slug: 'yummy-pizza' },
+//   { id: 9178, image: "", name: 'Sendvic', slug: 'sendvic' },
+//   { id: 9177, image: "", name: 'Fries', slug: 'fries' },
+// ];
 
-function Category() {
-  return (
-    <div className='container'>
-      <table className="table">
-    <thead>
-      <tr>
-        <th className="th">ID</th>
-        <th className="th">Image</th>
-        <th className="th">Name</th>
-        <th className="th">Slug</th>
-        <th className="th th-short"></th>
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((item, index) => (
-        <tr key={index}>
-          <td className="td">{item.id}</td>
-          <td className="td"><img src={item.image} alt="" /></td>
-          <td className="td">{item.name}</td>
-          <td className="td">{item.slug}</td>
-          <td className="buttons">
-          <button className="editButton"><img src="" alt="" /></button>
-          <button className="deleteButton"><img src="" alt="" /></button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-  </div>
-  )
-}
+// function Category() {
+//   return (
+//     <div className='container'>
+//       <table className="table">
+//     <thead>
+//       <tr>
+//         <th className="th">ID</th>
+//         <th className="th">Image</th>
+//         <th className="th">Name</th>
+//         <th className="th">Slug</th>
+//         <th className="th th-short"></th>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       {data.map((item, index) => (
+//         <tr key={index}>
+//           <td className="td">{item.id}</td>
+//           <td className="td"><img src={item.image} alt="" /></td>
+//           <td className="td">{item.name}</td>
+//           <td className="td">{item.slug}</td>
+//           <td className="buttons">
+//           <button className="editButton"><img src="" alt="" /></button>
+//           <button className="deleteButton"><img src="" alt="" /></button>
+//           </td>
+//         </tr>
+//       ))}
+//     </tbody>
+//   </table>
+//   </div>
+//   )
+// }
 
-export default Category
+// export default Category
--- src/Components/Category/SubNavCategory.jsx
+++ src/Components/Category/SubNavCategory.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import '../../Style/subNavCategory.css'
+// import React from 'react'
+// import '../../Style/subNavCategory.css'
 
-function SubNavCategory() {
-  return (
-    <div className='navbar'>
-  <div className='heading'>
-    <h1>Category</h1>
-  </div>
-  <div className='click'>
-    <button className='category'>+ADD CATEGORY</button>
-  </div>
- </div>
+// function SubNavCategory() {
+//   return (
+//     <div className='cat-nav'>
+//   <div className='heading'>
+//     <h1>Category</h1>
+//   </div>
+//   <div className='click'>
+//     <button className='category'>+ADD CATEGORY</button>
+//   </div>
+//  </div>
   
-  )
-}
+//   )
+// }
 
-export default SubNavCategory
+// export default SubNavCategory
