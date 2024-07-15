5a60cf1c675ccb117fe3260ec2d74f371ddcff97
Zibeydee <zibeydeceferli2012@gmail.com>
2024-07-15 14:26:55 +0400

    added reusable table for order history page

--- src/Components/OrderHistory/OrderHistory.jsx
+++ src/Components/OrderHistory/OrderHistory.jsx
@@ -1,79 +1,74 @@
-import React from 'react'
-import '../../Style/orderHistory.css'
+import React from "react";
+import "../../Style/orderHistory.css";
+import ReusableTable from "../Reusable/Table.jsx";
+
+
 
 function OrderHistory() {
+  const columns = [
+    {
+      key: 'id', title: 'ID', render: (text) => (
+        <div className="idContainer">
+          <p className="id">{text}</p>
+        </div>
+      )
+    },
+    {
+      key: 'customerId', title: 'Customer ID', render: (text) => (
+        <div className="customerID">
+          <p className="id">{text}</p>
+        </div>
+      )
+    },
+    { key: "time", title: "Time" },
+    { key: "deliveryAddress", title: "Delivery Address" },
+    { key: "amount", title: "Amount" },
+    { key: "paymentMethod", title: "Payment Method" },
+    { key: "contact", title: "Contact" },
+  ];
+
+
 
   const historyList = [
     {
-      id: '9177',
-      customerId: '022401',
-      time: '25 Dec 2021',
-      deliveryAddress: '29 Eve street',
-      amount: '$249.7',
-      paymentMethod: 'Cash on Delivery',
-      contact: '994-51-410-3130'
-
+      id: "9177",
+      customerId: "022401",
+      time: "25 Dec 2021",
+      deliveryAddress: "29 Eve street",
+      amount: "$249.7",
+      paymentMethod: "Cash on Delivery",
+      contact: "994-51-410-3130",
     },
     {
-      id: '9178',
-      customerId: '022401',
-      time: '25 Dec 2021',
-      deliveryAddress: '29 Eve street',
-      amount: '$249.7',
-      paymentMethod: 'Cash on Delivery',
-      contact: '994-51-410-3130'
+      id: "9178",
+      customerId: "022401",
+      time: "25 Dec 2021",
+      deliveryAddress: "29 Eve street",
+      amount: "$249.7",
+      paymentMethod: "Cash on Delivery",
+      contact: "994-51-410-3130",
     },
     {
-      id: '9179',
-      customerId: '022401',
-      time: '25 Dec 2021',
-      deliveryAddress: '29 Eve street',
-      amount: '$249.7',
-      paymentMethod: 'Cash on Delivery',
-      contact: '994-51-410-3130'
-    }
-  ]
+      id: "9179",
+      customerId: "022401",
+      time: "25 Dec 2021",
+      deliveryAddress: "29 Eve street",
+      amount: "$249.7",
+      paymentMethod: "Cash on Delivery",
+      contact: "994-51-410-3130",
+    },
+  ];
 
   return (
-    <div className='container'>
-      <div className='tableContainer'>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Customer ID</th>
-              <th>Time</th>
-              <th>Delivery Address</th>
-              <th>Amount</th>
-              <th>Payment Method</th>
-              <th>Contact</th>
-            </tr>
-          </thead>
-          <tbody>
-            {historyList.map((order, index) => (
-              <tr key={index}>
-                <td>
-                  <div className='idContainer'>
-                    <p>{order.id}</p>
-                  </div>
-                </td>
-                <td>
-                  <div className='customerID'>
-                    <p>{order.customerId}</p>
-                  </div>
-                </td>
-                <td>{order.time}</td>
-                <td>{order.deliveryAddress}</td>
-                <td>{order.amount}</td>
-                <td>{order.paymentMethod}</td>
-                <td>{order.contact}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div className="container">
+      <div className="tableContainer">
+        <ReusableTable
+          columns={columns}
+          data={historyList}
+          className="orderHistoryTable"
+        />
       </div>
     </div>
-  )
+  );
 }
-
-export default OrderHistory
+export default OrderHistory;
