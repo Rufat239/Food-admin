f50bd9a8cbe203ffe2e1b22d3c1bb9f96edc6f42
Alizadekh <aykhanalizadekh@gmail.com>
2024-07-18 19:52:58 +0400

    Adding authentication on login page for admins

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,23 +1,41 @@
-// src/Login.js
-import React from "react";
+import React, { useState } from "react";
 import "../Style/Login.css";
 import photo from "../assets/loginImages/login-illustration.svg";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/firebaseConfig";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/dashboardPage");
+    } catch (error) {
+      setError("Your password or email is incorrect!");
+    }
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
         <div className="login-content">
           <h2>Welcome Admin</h2>
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="input-group">
-              <label htmlFor="username"></label>
+              <label htmlFor="email"></label>
               <input
                 type="text"
-                id="username"
-                name="username"
-                placeholder="Username"
+                id="email"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="input-group">
@@ -27,17 +45,18 @@ const Login = () => {
                 id="password"
                 name="password"
                 placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <Link to="/dashboardPage">
-              <button type="submit" className="login-button">
-                Sign In
-              </button>
-            </Link>
+            <button type="submit" className="login-button">
+              Sign In
+            </button>
+            {error && <p className="error">{error}</p>}
           </form>
         </div>
         <div className="login-image">
-          <img src={photo} />
+          <img src={photo} alt="Login Illustration" />
         </div>
       </div>
     </div>
