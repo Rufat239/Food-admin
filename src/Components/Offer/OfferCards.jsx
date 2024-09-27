186249773f2e2f58770fa848fc86016691625038
NazifaG <gojayevanazifa@gmail.com>
2024-09-27 19:52:29 +0400

    Add lots of changes to OfferCard

--- src/Components/Offer/OfferCards.jsx
+++ src/Components/Offer/OfferCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../Style/offerCards.css";
 import Table from "../Reusable/Table.jsx";
 import Delete from "../../assets/SVG/delete.svg";
@@ -6,34 +6,173 @@ import Edit from "../../assets/SVG/edit.svg";
 // import pizza from '../../assets/offerImages/pizza.png';
 import pizza from "../../NavbarImages/Background.png";
 import DeleteModal from "../DeleteModal/DeleteModal.jsx";
+import getOffers from "../../service/offers/getAllOffer.js";
+import axios from "axios";
+import Loading from "../Loading/Loading.jsx";
+import Form from '../form/Form'
+import SideBar from '../sideBar/SideBar';
+import updatedOffer from "../../service/offers/updateOffer.js";
+import deletedOffer from "../../service/offers/deleteOffer.js";
+
+
 function OfferCards() {
-  const [showDeleteModal, setShowDeleteModal] = useState(false)
-  const offers = [
-    {
-      id: 9177,
-      image: pizza,
-      title: "Do you like Pizza at Papa John's?",
-      description: "Yummy this pizza but...",
+  // responsive dizayn ucun inline 
+  const isSmallScreen = window.innerWidth <= "431px";
+
+  // For delete ID
+  const [deleteID, setDeleteID] =useState("")
+// For getting ID
+  const[offerID, setOfferID] = useState("")
+
+                        // This codes using for Modal
+  const [showSideBar, setShowSideBar] = useState(false);
+  const [formData, setFormData] =useState({
+      title: "",
+      description: "",
+  })
+
+  const objectWithSchema = {
+    data: formData,
+    //           {
+    //   // image: "",
+    //   title: "",
+    //   description: "",
+     
+    // },
+    schema: {
+      // image: { type: "file", label: "Image" },
+      title: { type: "text", label: "Title" },
+      description: { type: "textarea", label: "Description" },
+     
+   
     },
-    {
-      id: 9178,
-      image: pizza,
-      title: "Do you like Pizza at Papa John's?",
-      description: "Yummy this pizza but...",
-    },
-    {
-      id: 9179,
-      image: pizza,
-      title: "Do you like Pizza at Papa John's?",
-      description: "Yummy this pizza but...",
-    },
-  ];
+  };
+
+  const openSideBar = () => {
+    setShowSideBar(true);
+    document.body.style.overflow = "hidden";
+  };
+  const closeSideBar = () => {
+    setShowSideBar(false);
+    document.body.style.overflow = "auto";
+  };
+
+                          // This codes using for getting data
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  // async function getOffers() {
+  //   const response = await axios.get(
+  //     // `https://test-foody-admin-default-rtdb.firebaseio.com/offers.json`
+  //     `https://test-foody-admin-default-rtdb.firebaseio.com/offers.json`
+  //   );
+    
+
+  //   setData(Object.entries(response.data).map(([id, offer]) => ({
+  //     id: id,
+  //     ...offer,
+  //   })));
+  //   setLoading(true)
+  // }
+
+
+  // useEffect(() => {
+  //   getOffers();
+  // }, []);
+
+
+
+  // Second option for fetching data
+async function getOffers() {
+  try {
+    const response = await axios.get(
+      `https://test-foody-admin-default-rtdb.firebaseio.com/offers.json`
+    );
+
+    // If response.data is empty or null control it
+    if (response.data) {
+      // If data have Object.entries works
+      setData(Object.entries(response.data).map(([id, offer]) => ({
+        id: id,
+        ...offer,
+      })));
+    } else {
+      // If data empty,update setData with empty array  
+      setData([]);
+      console.log("No offers available");
+    }
+    
+    
+    
+  } catch (error) {
+    console.error("Error fetching offers:", error);
+    setData([]); 
+  } finally {
+    setLoading(true); 
+  }
+}
+
+useEffect(() => {
+  getOffers();
+}, []);
+
+
+// Third option for fetching data
+// useEffect(() => {
+//   const fetchOffers = async () => {
+//     // setLoading(true);
+//     try {
+//       const fetchedData = await getOffers();
+//       console.log("Firebase Data:", data);
+      
+//       // if (fetchedData && typeof fetchedData === 'object')
+//       if(data){
+//         const formattedData = Object.entries(data).map(([id, offer]) => ({
+//           id: id,
+//           ...offer,
+//         }));
+//         console.log("Formatted Offers:", formattedData);
+//         setData(formattedData);
+//       } else {
+//         console.error("No valid data fetched");
+//       }
+//     } catch (error) {
+//       console.error("Error while fetching offers:", error);
+//     }
+//     // setLoading(false);
+//   };
+
+//   fetchOffers();
+// }, []);
+
+
+
+// for deleting product with id
+
+ async function deleteOffer(){
+   setShowDeleteModal(false)
+ 
+  await deletedOffer(deleteID)
+  window.location.reload()
+
+
+  // const success = await deletedOffer(deleteID)
+  // if (success) {
+  //   setData((prevData)=> prevData.filter(res=>res.id !== deleteID))
+  // } else {
+  //   console.log("error");
+    
+  // }
+
+  }
+
   const handleShowDeleteModal = (id) => {
-    setShowDeleteModal(id)
-  }
-  const handleCancel = () => {
-    setShowDeleteModal(false)
-  }
+    setDeleteID(id)
+     setShowDeleteModal(true)
+ };
+
   const columns = [
     {
       key: "id",
@@ -45,7 +184,7 @@ function OfferCards() {
       ),
     },
     {
-      key: "image",
+      key: "url",
       title: "Image",
       render: (text) => <img className="productImageOffer" src={text} alt="" />,
     },
@@ -64,23 +203,57 @@ function OfferCards() {
       title: "",
       render: (text, record) => (
         <div className="actions">
-          <button className="edit">
-            <img src={Edit} alt="Edit" />
+          <button className="editOffer"  onClick={()=>{
+
+       openSideBar()
+       setOfferID(record.id);
+       
+          }}>
+            <img src={Edit} alt="Edit" className="editImg1" />
           </button>
-          <button className="delete" onClick={() => handleShowDeleteModal(record.id)}>
-            <img src={Delete} alt="Delete" />
+          <button
+            className="deleteOffer"
+            onClick={() => handleShowDeleteModal(record.id)}
+          >
+            <img src={Delete} alt="Delete" className="deleteImg1" />
           </button>
         </div>
-      )
+      ),
     },
   ];
+
+  // console.log(data);
+
   return (
     <div className="offerContainer1">
-      <Table columns={columns} data={offers} className="offerTable" />
-      {showDeleteModal  && (
-            <DeleteModal
-            onCancel = {handleCancel}/>
-          )}
+      {loading ? (
+        <>
+        {data.length >0 ? (
+             <Table columns={columns} data={data} className="offerTable" />
+        ):(
+          <p style={{ position: "absolute", color: "#C035A2", 
+            fontSize: isSmallScreen ? "17px" : "20px", fontFamily: "Roboto", left: isSmallScreen ? "35%" :"55%", top: "350px"}}>
+            No offers available</p>
+        )}
+         
+          <SideBar Show={showSideBar} onClose={closeSideBar}>
+            <Form
+              objectWithSchema={objectWithSchema}
+              title="Edit Offer"
+              upload="your offer"
+              subtitle="Edit your Offer information"
+              page="Update Offer"
+              onClose={closeSideBar}
+              isEdit = {true}
+              offerId = {offerID}
+              formType="updatedOffer"
+            />
+          </SideBar>
+          {showDeleteModal && <DeleteModal onCancel={() => setShowDeleteModal(false)} deleted={deleteOffer}/>}
+        </>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 }
